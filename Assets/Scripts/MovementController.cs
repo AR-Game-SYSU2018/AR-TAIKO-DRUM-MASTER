6d13896f9032b98589cb19b969c9779bc0232891
qianzixuan <qianzixuan1996@gmail.com>
2018-04-29 23:12:34 +0800

    correct moving speed

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -8,6 +8,7 @@ public class MovementController : MonoBehaviour {
     private Rigidbody rb;
     private System.DateTime begintime;
     private float step;
+    private System.DateTime lastUpdateTime;
 
 	// Use this for initialization
 	void Start () {
@@ -16,29 +17,33 @@ public class MovementController : MonoBehaviour {
         scoreboard = GameObject.Find("ScoreBoard");
         theBall.GetComponent<TextMesh>().text = "***";
         begintime = System.DateTime.Now;
-        step = 1.044f;
+        step = 1.56695f;
+        lastUpdateTime = System.DateTime.Now;
     }
 	
 	// Update is called once per frame
     void Update ()
     {
-        Move();
+        System.DateTime tmp = System.DateTime.Now;
+        double gap = (tmp - lastUpdateTime).TotalMilliseconds;
+        lastUpdateTime = tmp;        
+        Move(step * gap * 3.0 / 50.0 );
     }
 
-	void Move ()
+	void Move (double step_)
     {
-        if (rb.position.z < -7.5f)
+        if (rb.position.z < -14.1025f)
         {
             System.DateTime tmp = System.DateTime.Now;
             scoreboard.GetComponent<TextMesh>().text = "" + ((tmp-begintime).TotalMilliseconds);
-            //rb.MovePosition(new Vector3(-16.7f, 0f, 95.9f));
-            GameObject.Destroy(theBall);
+            rb.MovePosition(new Vector3(-16.7f, 0f, 133.5225f));
+            //GameObject.Destroy(theBall);
             //theBall.name.
         }
         Vector3 movement = new Vector3();
-        movement.Set(rb.position.x, rb.position.y, rb.position.z-(step));
+        movement.Set(rb.position.x, rb.position.y, rb.position.z-(float)(step_));
         rb.MovePosition(movement);
-        //theBall.GetComponent<TextMesh>().text = movement.ToString();
+        theBall.GetComponent<TextMesh>().text = movement.ToString();
 
         //theBall.GetComponent<TextMesh>().text = ""+(datetime.Millisecond-begintime);
 
